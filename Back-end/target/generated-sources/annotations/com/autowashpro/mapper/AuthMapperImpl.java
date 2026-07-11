956cff85aea5717e93ab2e61e8d6671e504c42c5
Duc Anh <nguyentranducanh220605@gmail.com>
2026-07-11 14:49:53 +0700

    update booking

--- Back-end/target/generated-sources/annotations/com/autowashpro/mapper/AuthMapperImpl.java
+++ Back-end/target/generated-sources/annotations/com/autowashpro/mapper/AuthMapperImpl.java
@@ -11,8 +11,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-06-27T10:32:13+0700",
-    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.12 (Oracle Corporation)"
+    date = "2026-07-11T14:43:10+0700",
+    comments = "version: 1.5.5.Final, compiler: Eclipse JDT (IDE) 3.46.100.v20260624-0231, environment: Java 21.0.11 (Eclipse Adoptium)"
 )
 @Component
 public class AuthMapperImpl implements AuthMapper {
@@ -61,9 +61,9 @@ public class AuthMapperImpl implements AuthMapper {
         LoginCustomerResponse loginCustomerResponse = new LoginCustomerResponse();
 
         loginCustomerResponse.setName( customer.getFullName() );
+        loginCustomerResponse.setAccumulatedPoints( customer.getAccumulatedPoints() );
         loginCustomerResponse.setPhone( customer.getPhone() );
         loginCustomerResponse.setTier( customer.getTier() );
-        loginCustomerResponse.setAccumulatedPoints( customer.getAccumulatedPoints() );
 
         loginCustomerResponse.setId( String.valueOf(customer.getCustomerId()) );
         loginCustomerResponse.setTotalSpend( customer.getTotalSpent().longValue() );
