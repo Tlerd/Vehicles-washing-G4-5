e4c0dd4d86287e371754675f32897832a9ec7008
Duc Anh <nguyentranducanh220605@gmail.com>
2026-07-22 17:01:04 +0700

    feat: add PayOS config properties

---
+++ Back-end/src/main/java/com/autowashpro/config/PayOsProperties.java
@@ -0,0 +1,80 @@
+package com.autowashpro.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "autowash.payos")
+public class PayOsProperties {
+
+    private String clientId;
+    private String apiKey;
+    private String checksumKey;
+    private String baseUrl;
+    private String returnUrl;
+    private String cancelUrl;
+    private long connectTimeoutMs;
+    private long readTimeoutMs;
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
+    }
+
+    public String getApiKey() {
+        return apiKey;
+    }
+
+    public void setApiKey(String apiKey) {
+        this.apiKey = apiKey;
+    }
+
+    public String getChecksumKey() {
+        return checksumKey;
+    }
+
+    public void setChecksumKey(String checksumKey) {
+        this.checksumKey = checksumKey;
+    }
+
+    public String getBaseUrl() {
+        return baseUrl;
+    }
+
+    public void setBaseUrl(String baseUrl) {
+        this.baseUrl = baseUrl;
+    }
+
+    public String getReturnUrl() {
+        return returnUrl;
+    }
+
+    public void setReturnUrl(String returnUrl) {
+        this.returnUrl = returnUrl;
+    }
+
+    public String getCancelUrl() {
+        return cancelUrl;
+    }
+
+    public void setCancelUrl(String cancelUrl) {
+        this.cancelUrl = cancelUrl;
+    }
+
+    public long getConnectTimeoutMs() {
+        return connectTimeoutMs;
+    }
+
+    public void setConnectTimeoutMs(long connectTimeoutMs) {
+        this.connectTimeoutMs = connectTimeoutMs;
+    }
+
+    public long getReadTimeoutMs() {
+        return readTimeoutMs;
+    }
+
+    public void setReadTimeoutMs(long readTimeoutMs) {
+        this.readTimeoutMs = readTimeoutMs;
+    }
+}
